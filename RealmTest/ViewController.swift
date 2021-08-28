9c27ba34051eeba4db6cf0b2179b2bd3e73cf812
Berezentsev Evgeny <jonn09@icloud.com>
2021-08-28 11:54:00 +0300

    Initial Commit

---
+++ RealmTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmTest
+//
+//  Created by Евгений Березенцев on 28.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
